dcd1c47b8d8ecc85944f979266b5b10498e6453a
Cyril Jacquesson <cyril@jacquesson.me>
2018-12-06 14:58:48 +0100

    2.2.1

--- gulpfile.js/modules/default.js
+++ gulpfile.js/modules/default.js
@@ -6,7 +6,7 @@ const tasks = require("./manager");
 gulp.task(
   "default",
   gulp.series(
-    gulp.parallel(tasks.sprites, tasks.fonts),
+    gulp.parallel(tasks.sprites, tasks.fonts, tasks.svgstore),
     tasks.images,
     gulp.parallel(tasks.pug, tasks.sass, tasks.javascript),
     tasks.browsersync
