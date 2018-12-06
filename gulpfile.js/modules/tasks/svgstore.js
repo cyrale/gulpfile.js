71ebf9a6e1e6dba0017f6e0f0a8cf17545743f4b
Cyril Jacquesson <cyril@jacquesson.me>
2018-12-06 14:54:21 +0100

    2.1.5

---
+++ gulpfile.js/modules/tasks/svgstore.js
@@ -0,0 +1,61 @@
+"use strict";
+
+const path = require("path");
+
+const gulp = require("gulp");
+const plumber = require("gulp-plumber");
+const rename = require("gulp-rename");
+const svgstore = require("gulp-svgstore");
+const svgmin = require("gulp-svgmin");
+const newer = require("gulp-newer");
+
+const bsync = require("./browsersync");
+
+const notify = require("../notify");
+
+const Task = require("../task");
+
+class SVGStore extends Task {
+  constructor(name, options) {
+    super(name, options);
+
+    this.options.settings = this.options.settings || {};
+  }
+
+  build() {
+    return gulp
+      .src(this.options.src, { cwd: this.options.cwd })
+      .pipe(newer(path.join(this.options.cwd, this.options.dst)))
+      .pipe(
+        plumber(error => {
+          notify.onError(error, this.name);
+        })
+      )
+      .pipe(
+        svgmin(file => ({
+          plugins: [
+            {
+              removeViewBox: true
+            },
+            {
+              cleanupIDs: {
+                prefix: path.basename(file.relative, path.extname(file.relative)) + "-",
+                minify: true
+              }
+            }
+          ]
+        }))
+      )
+      .pipe(svgstore())
+      .pipe(
+        rename({
+          basename: path.basename(this.options.filename, path.extname(this.options.filename)),
+          extname: ".svg"
+        })
+      )
+      .pipe(gulp.dest(this.options.dst, { cwd: this.options.cwd }))
+      .pipe(bsync.sync());
+  }
+}
+
+module.exports = SVGStore;
