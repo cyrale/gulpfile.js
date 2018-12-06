dca921d3a65572bac97b3fa0fab3925c755a75fe
Cyril Jacquesson <cyril@jacquesson.me>
2018-12-06 14:40:38 +0100

    2.0.2

--- gulpfile.js/index.js
+++ gulpfile.js/index.js
@@ -6,4 +6,7 @@ const requireDir = require('require-dir');
 // Fallback for windows backs out of node_modules folder to root of project
 process.env.PWD = process.env.PWD || path.resolve(process.cwd(), '../../');
 
+// Change working directory
+process.chdir(process.env.PWD);
+
 requireDir('./modules', {recurse: true});
