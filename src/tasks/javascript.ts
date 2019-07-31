503b4098206d9d21a00204106a1273717c66c247
Cyril Jacquesson <cyril@jacquesson.me>
2019-07-31 15:27:41 +0200

    Build Javascript files and display error like ESLint

--- src/tasks/javascript.ts
+++ src/tasks/javascript.ts
@@ -1,7 +1,24 @@
-import GulpESLint from "gulp-eslint";
+import path from "path";
 
+import { dest } from "gulp";
+
+import GulpBabel from "gulp-babel";
+import GulpConcat from "gulp-concat";
+import GulpESLint from "gulp-eslint";
+import GulpIf from "gulp-if";
+import GulpRename from "gulp-rename";
+import GulpUglify from "gulp-uglify";
+
+import { CLIEngine, Linter } from "eslint";
 import Browsersync from "./browsersync";
 import Task, { IGulpOptions } from "./task";
+import Severity = Linter.Severity;
+
+export const babelDefaultSettings: {
+  [name: string]: any;
+} = {
+  presets: ["@babel/preset-env"],
+};
 
 export default class Javascript extends Task {
   public static readonly taskName: string = "javascript";
@@ -16,6 +33,22 @@ export default class Javascript extends Task {
   }
 
   public buildSpecific(stream: NodeJS.ReadWriteStream, options: IGulpOptions): NodeJS.ReadWriteStream {
+    const babelActive = typeof this.settings.settings.babel === "object" || this.settings.settings.babel !== false;
+
+    let babelSettings: {} = {};
+    if (babelActive && typeof this.settings.settings.babel === "object") {
+      babelSettings = { ...babelDefaultSettings, ...this.settings.settings.babel };
+    }
+
+    stream
+      .pipe(GulpIf(babelActive, GulpBabel(babelSettings)))
+      .pipe(GulpConcat(this.settings.filename))
+      .pipe(dest(this.settings.dst, options))
+      .pipe(Browsersync.getInstance().sync(this.browserSyncSettings) as NodeJS.ReadWriteStream)
+      .pipe(GulpUglify({ mangle: false }))
+      .pipe(GulpRename({ suffix: ".min" }))
+      .pipe(dest(this.settings.dst, options));
+
     return stream;
   }
 
@@ -24,11 +57,40 @@ export default class Javascript extends Task {
       .pipe(GulpESLint())
       .pipe(GulpESLint.format())
       .pipe(
-        GulpESLint.results((filesWithErrors: any[]): void => {
-          this.lintError = filesWithErrors.errorCount > 0;
+        GulpESLint.results((filesWithErrors: { errorCount: number }): void => {
+          // this.lintError = filesWithErrors.errorCount > 0;
         })
       );
 
     return stream;
+  }
+
+  protected displayError(error: any): void {
+    const cliEngine = new CLIEngine({});
+    const formatter = cliEngine.getFormatter("stylish");
+    const relativeFile = path.relative(this.settings.cwd, error.fileName);
+
+    console.log(
+      formatter([
+        {
+          errorCount: 1,
+          filePath: relativeFile,
+          fixableErrorCount: 0,
+          fixableWarningCount: 0,
+          messages: [
+            {
+              column: error.loc.column,
+              line: error.loc.line,
+              message: error.message.replace(error.fileName, relativeFile),
+              nodeType: "",
+              ruleId: null,
+              severity: 2 as Severity,
+              source: null,
+            },
+          ],
+          warningCount: 0,
+        },
+      ])
+    );
   }
 }
