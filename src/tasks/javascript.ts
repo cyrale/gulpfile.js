b2d311326bb612a2adcbf26da40f31ac0625c2f1
Cyril Jacquesson <cyril@jacquesson.me>
2019-07-31 13:37:22 +0200

    Install dependencies for Javascript tasks

--- src/tasks/javascript.ts
+++ src/tasks/javascript.ts
@@ -3,7 +3,16 @@ import Task from "./task";
 export default class Javascript extends Task {
   public static readonly taskName: string = "javascript";
 
-  public buildSpecific(stream: NodeJS.ReadWriteStream): NodeJS.ReadWriteStream {
+  constructor(name: string, settings: object) {
+    super(name, settings);
+
+    this.withSourcemaps = true;
+
+    this.defaultDest = false;
+    this.browserSyncSettings = { match: "**/*.js" };
+  }
+
+  public buildSpecific(stream: NodeJS.ReadWriteStream, options: IGulpOptions): NodeJS.ReadWriteStream {
     return stream;
   }
 
