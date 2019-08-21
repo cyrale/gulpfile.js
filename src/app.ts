e195983fc67e44b65201af4cc6d7ce3bf3189f2e
Cyril Jacquesson <cyril@jacquesson.me>
2019-08-21 12:07:20 +0200

    Fix root directory for Windows

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,7 @@ import process from "process";
 
 try {
   // Fallback for Windows backs out of node_modules folder to root of project.
-  process.env.PWD = process.env.PWD || path.resolve(process.cwd(), "../../");
+  process.env.PWD = process.env.PWD || path.resolve(process.cwd(), "../../../");
 
   // Change working directory.
   process.chdir(process.env.PWD as string);
