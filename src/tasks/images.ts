93c3be90bcfac029f8c8767162ce781645fb26e7
Cyril Jacquesson <cyril@jacquesson.me>
2019-10-18 14:17:16 +0200

    Add WebP support in image minification

--- src/tasks/images.ts
+++ src/tasks/images.ts
@@ -2,11 +2,17 @@ import chalk from "chalk";
 import del from "del";
 import log from "fancy-log";
 import fs from "fs";
+import filter from "gulp-filter";
 import imagemin from "gulp-imagemin";
 import newer from "gulp-newer";
+import rename from "gulp-rename";
+import imageminWebP from "imagemin-webp";
 import merge from "lodash/merge";
+import mergeStream from "merge-stream";
+import minimatch from "minimatch";
 import path from "path";
 
+import { IBuildSettings } from "./task";
 import TaskExtended from "./task-extended";
 
 /**
@@ -26,6 +32,14 @@ export default class Images extends TaskExtended {
    * @readonly
    */
   public static readonly taskOrder: number = 30;
+
+  /**
+   * List of supported extension for WebP conversion.
+   * @type {string[]}
+   * @private
+   * @readonly
+   */
+  private readonly _webPSupportedExtension: string[] = ["**/*.jpg", "**/*.jpeg", "**/*.png"];
 
   /**
    * Task constructor.
@@ -56,29 +70,57 @@ export default class Images extends TaskExtended {
     };
 
     this._settings.settings = merge(defaultSettings, this._settings.settings || {});
+
+    const webpDefaultSettings: {} = {};
+
+    if (typeof this._settings.settings.webp === "object") {
+      this._settings.settings.webp = merge(webpDefaultSettings, this._settings.settings.webp);
+    } else {
+      this._settings.settings.webp = !!this._settings.settings.webp ? webpDefaultSettings : false;
+    }
   }
 
   /**
    * Method to add specific steps for the build.
    *
    * @param {NodeJS.ReadWriteStream} stream
+   * @param {IBuildSettings} buildSettings
    * @return {NodeJS.ReadWriteStream}
    * @protected
    */
-  protected _buildSpecific(stream: NodeJS.ReadWriteStream): NodeJS.ReadWriteStream {
-    return stream
-      .pipe(newer(path.resolve(this._settings.cwd, this._settings.dst)))
-      .pipe(
-        imagemin(
-          [
-            imagemin.jpegtran(this._settings.settings.jpegtran || {}),
-            imagemin.optipng(this._settings.settings.optipng || {}),
-            imagemin.gifsicle(this._settings.settings.gifsicle || {}),
-            imagemin.svgo(this._settings.settings.svgo || {}),
-          ],
-          { verbose: true }
-        )
-      );
+  protected _buildSpecific(stream: NodeJS.ReadWriteStream, buildSettings: IBuildSettings): NodeJS.ReadWriteStream {
+    const streams: NodeJS.ReadWriteStream[] = [];
+
+    stream = stream.pipe(newer(path.resolve(this._settings.cwd, this._settings.dst)));
+
+    const streamImageMin = stream.pipe(
+      imagemin(
+        [
+          imagemin.jpegtran(this._settings.settings.jpegtran || {}),
+          imagemin.optipng(this._settings.settings.optipng || {}),
+          imagemin.gifsicle(this._settings.settings.gifsicle || {}),
+          imagemin.svgo(this._settings.settings.svgo || {}),
+        ],
+        { verbose: true }
+      )
+    );
+
+    streams.push(streamImageMin);
+
+    if (this._settings.settings.webp) {
+      const streamWebP = stream
+        .pipe(filter(this._webPSupportedExtension))
+        .pipe(imagemin([imageminWebP(this._settings.settings.webp)]))
+        .pipe(
+          rename((pPath: rename.ParsedPath): void => {
+            pPath.extname += ".webp";
+          })
+        );
+
+      streams.push(streamWebP);
+    }
+
+    return mergeStream(streams);
   }
 
   /**
@@ -109,12 +151,39 @@ export default class Images extends TaskExtended {
       }
 
       newFilename = path.join(newFilename, path.basename(filename));
+      this._deleteFile(newFilename);
 
-      log("gulp-imagemin: Deleted image: " + chalk.blue(path.relative(this._settings.cwd, newFilename)));
-
-      del.sync(newFilename, {
-        force: true,
-      });
+      if (this._settings.settings.webp && this._webPSupportedFile(newFilename)) {
+        this._deleteFile(newFilename + ".webp");
+      }
     });
+  }
+
+  /**
+   * Simply delete a file.
+   *
+   * @param {string} filename
+   * @private
+   */
+  private _deleteFile(filename: string): void {
+    log("gulp-imagemin: Deleted image: " + chalk.blue(path.relative(this._settings.cwd, filename)));
+
+    del.sync(filename, {
+      force: true,
+    });
+  }
+
+  /**
+   * Check if a file is supported for WebP conversion.
+   *
+   * @param {string} filename
+   * @return {boolean}
+   * @private
+   */
+  private _webPSupportedFile(filename: string): boolean {
+    return (this._webPSupportedExtension as any[]).reduce(
+      (acc: boolean, ext: string): boolean => acc || minimatch(filename, ext),
+      false
+    );
   }
 }
