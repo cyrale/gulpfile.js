d5bf5fa8044fb5f82c440976771e45b550a2f991
Cyril Jacquesson <cyril@jacquesson.me>
2019-11-08 19:20:49 +0100

    Rename test to examples

---
+++ examples/src/js/webpack/app.js
@@ -0,0 +1,7 @@
+// import _ from "lodash";
+import { sayHello } from "./greet";
+
+// console.log(_);
+
+const name = "Webpack";
+console.log(sayHello(name));
---
+++ examples/src/typescript/main.ts
@@ -0,0 +1,4 @@
+import { sayHello } from "./greet";
+
+const name = "TypeScript";
+console.log(sayHello(name));
