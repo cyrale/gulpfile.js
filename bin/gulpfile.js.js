a7873a6499980d548a6bf52a103407e4699021ca
Cyril Jacquesson <cyril@jacquesson.me>
2020-02-13 12:13:53 +0100

    Pass arguments to the main process

--- bin/gulpfile.js.js
+++ bin/gulpfile.js.js
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 
-const additionalArgs = require("minimist")(process.argv.slice(2))._;
 let args = ["--gulpfile", "node_modules/gulpfile.js/dist/app.js"];
 
+const additionalArgs = process.argv.slice(2);
 if (additionalArgs.length) {
-  args = args.concat(additionalArgs);
+  args = [...args, ...additionalArgs];
 }
 
 require("child_process").fork("node_modules/gulp/bin/gulp", args);
